c82fccfac6307de5eba7fb3b36fc4f9f66e71f2c
tpssim <teemu.simola@outlook.com>
2021-02-22 19:49:15 +0200

    endpoint for adding new guilds

--- src/routes/guilds.ts
+++ src/routes/guilds.ts
@@ -1,6 +1,8 @@
 import router from 'express-promise-router';
 import { Request, Response } from 'express';
 import { getGuildById, getGuilds, searchGuilds } from '../data/guilds';
+import { ulid } from 'ulid';
+import { DBObject } from '../middleware/data';
 
 const guildRoutes = router();
 
@@ -9,6 +11,31 @@ guildRoutes.get('/', async (req: Request, res: Response) => {
     const result = await getGuilds(db);
     res.status(200).send(result);
 });
+
+interface RequestBody {
+    name: string;
+    subject: string;
+    city: string;
+}
+
+guildRoutes.post('/', async (req: Request, res: Response) => {
+    const guildBody: RequestBody = req.body;
+    const db: DBObject = req.app.locals.db;
+    
+    if ('name' in guildBody) {
+        const oldGuilds = await getGuilds(db);
+        const newGuilds = oldGuilds.concat({
+            name: guildBody.name,
+            city: guildBody.city,
+            subject:guildBody.subject,
+            id: ulid()
+        })
+        db.setGuilds(newGuilds);
+        return res.status(201).send(newGuilds);
+    }
+
+    return res.status(406).send('Request body was bad');
+})
 
 guildRoutes.get('/:id', async (req: Request, res: Response) => {
     const db = req.app.locals.db;
